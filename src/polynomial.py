0a89dde34571794798f8d4f3a731d7de71e0fb73
Milan Nikic <milnik79@gmail.com>
2017-01-13 11:34:58 +0100

    Added method for user readable string representation.

--- src/polynomial.py
+++ src/polynomial.py
@@ -6,3 +6,42 @@ class Polynomial(object):
 
     def __repr__(self):
         return 'Polynomial(%r, %r)' % (self.coefficients, self.variable)
+
+    def __str__(self):
+        """
+        Convert list of coefficients into a user readable string.
+        :return:
+        """
+        result = []
+        for i in range(len(self.coefficients)-1, -1, -1):
+            if self.coefficients[i]:
+                if i < len(self.coefficients) - 1:
+                    if self.coefficients[i] >= 0:
+                        result.append('+')
+                    else:
+                        result.append('-')
+                    result.append(self.__to_string_monomial(abs(self.coefficients[i]), i))
+                else:
+                    result.append(self.__to_string_monomial(self.coefficients[i], i))
+        return ' '.join(result)
+
+    def __to_string_monomial(self, coefficient, power):
+        """
+        Convert a monomial to string.
+        :param coefficient: coefficient of monomial
+        :param power: power of monomial
+        :return: String in form coefficientx^power
+        """
+        if power == 1:
+            if coefficient == 1:
+                return 'x'
+            elif coefficient == -1:
+                return '-x'
+            return '%sx' % coefficient
+        elif power:
+            if coefficient == 1:
+                return 'x^%d' % power
+            elif coefficient == -1:
+                return '-x^%d' % power
+            return '%sx^%d' % (coefficient, power)
+        return "%s" % coefficient
--- test/test_polynomial.py
+++ test/test_polynomial.py
@@ -5,10 +5,20 @@ from src.polynomial import Polynomial
 
 class TestPolynomial(TestCase):
     p1 = None
+    p2 = None
+    p3 = None
 
     def setUp(self):
         self.p1 = Polynomial([5, 4, 3, 2, 1], 5)
+        self.p2 = Polynomial([1], None)
+        self.p3 = Polynomial([1, 0, 0, -5, 8], 10)
 
     def test_representation_polynomial(self):
-        representation = repr(self.p1)
-        self.assertEqual('Polynomial([5, 4, 3, 2, 1], 5)', representation)
+        self.assertEqual('Polynomial([5, 4, 3, 2, 1], 5)', repr(self.p1))
+        self.assertEqual('Polynomial([1], None)', repr(self.p2))
+        self.assertEqual('Polynomial([1, 0, 0, -5, 8], 10)', repr(self.p3))
+
+    def test_string_polynomial(self):
+        self.assertEqual('x^4 + 2x^3 + 3x^2 + 4x + 5', str(self.p1))
+        self.assertEqual('1', str(self.p2))
+        self.assertEqual('8x^4 - 5x^3 + 1', str(self.p3))
